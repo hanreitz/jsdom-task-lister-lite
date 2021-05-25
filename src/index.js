33a057168b2fea9a102aad214c8b33afc783132d
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-05-25 17:30:08 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.getElementById('create-task-form');
+  const taskDescription = document.getElementById('new-task-description');
+  const taskList = document.querySelector('#list ul#tasks');
+
+  document.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let newTask = document.createElement('li');
+    newTask.innerText = taskDescription.value;
+    taskList.appendChild(newTask);
+  })
 });
